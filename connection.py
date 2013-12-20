85c0944f128fb83267450193c88d93f140d4447e
Matt Keeter <matt.j.keeter@gmail.com>
2013-12-20 11:20:41 -0500

    beginnings of drag+drop connections

---
+++ connection.py
@@ -0,0 +1,58 @@
+import wx
+
+class Connection(wx.Control):
+    def __init__(self, parent, origin):
+        super(Connection, self).__init__(parent, size=(10, 10))
+        self.origin = origin
+        self.destination = None
+
+        # Get mouse position within the canvas object.
+        self.mouse_pos = self.origin_pos = (
+            origin.GetPosition() +
+            origin.Parent.GetPosition() + wx.Point(5, 5))
+
+        self.Bind(wx.EVT_PAINT, self.draw)
+        self.Bind(wx.EVT_LEFT_UP, self.release)
+        self.Bind(wx.EVT_MOTION, self.on_motion)
+
+        self.update()
+
+    def on_motion(self, event):
+        if not self.destination:
+            self.mouse_pos = (
+                    event.GetPosition() +
+                    self.origin.GetPosition() +
+                    self.origin.Parent.GetPosition())
+            self.update()
+
+    def draw(self, event):
+        dc = wx.PaintDC(self)
+        dc.SetPen(wx.Pen(self.origin.color, 4))
+        if ((self.mouse_pos.x > self.origin_pos.x) ^
+            (self.mouse_pos.y > self.origin_pos.y)):
+            dc.DrawLine(5, self.Size.y - 5, self.Size.x - 5, 5)
+        else:
+            dc.DrawLine(5, 5, self.Size.x - 5, self.Size.y - 5)
+
+    def get_bounds(self):
+        xmin = min(self.mouse_pos.x - 5, self.origin_pos.x - 5)
+        ymin = min(self.mouse_pos.y - 5, self.origin_pos.y - 5)
+        xmax = max(self.mouse_pos.x + 5, self.origin_pos.x + 5)
+        ymax = max(self.mouse_pos.y + 5, self.origin_pos.y + 5)
+        return xmin, ymin, xmax, ymax
+
+    def update(self):
+        if not self.destination:
+            xmin, ymin, xmax, ymax = self.get_bounds()
+            self.Size = (xmax - xmin, ymax - ymin)
+            self.MoveXY(xmin, ymin)
+
+    def release(self, event):
+        if not self.destination:
+            wx.CallAfter(self.Destroy)
+
+
+
+
+
+current = None
--- io.py
+++ io.py
@@ -2,6 +2,7 @@ import wx
 import name
 
 import colors
+import connection
 
 _colors = {
         name.Name:  colors.yellow,
@@ -14,19 +15,10 @@ class IO(wx.Control):
 
         self.datum = datum
         self.color = _colors.get(self.datum.type, colors.red)
+        self.region = wx.Region(0, 0, 10, 10)
 
         self.SetBackgroundColour(self.color)
-        self.Bind(wx.EVT_MOTION, self.on_motion)
-        self.Bind(wx.EVT_LEAVE_WINDOW, self.on_leave_window)
 
-    def on_motion(self, event):
-        self.SetBackgroundColour(
-                [min(255, c + 75) for c in self.color])
-        self.Refresh()
-
-    def on_leave_window(self, event):
-        self.SetBackgroundColour(self.color)
-        self.Refresh()
 
 class Input(IO):
     def __init__(self, parent, datum):
@@ -35,3 +27,40 @@ class Input(IO):
 class Output(IO):
     def __init__(self, parent, datum):
         super(Output, self).__init__(parent, datum)
+        self.Bind(wx.EVT_LEFT_DOWN,    self.on_click)
+        self.Bind(wx.EVT_LEFT_UP,      self.on_release)
+        self.Bind(wx.EVT_MOTION,       self.on_motion)
+        self.Bind(wx.EVT_LEAVE_WINDOW, self.on_motion)
+        self.connection = None
+
+    def on_click(self, event):
+        print self.Parent.Parent
+        self.connection = connection.Connection(self.Parent.Parent, self)
+        wx.PostEvent(self.connection, event)
+
+    def on_release(self, event):
+        if self.connection.destination is None:
+            wx.PostEvent(self.connection, event)
+            return
+        self.connection = None
+        event.Skip()
+
+    def on_motion(self, event):
+
+        # First, check to see if we should be forwarding events to a
+        # connection object that's being created.
+        if self.connection:
+            if self.connection.destination is None:
+                wx.PostEvent(self.connection, event)
+                return
+            else:
+                self.connection = None
+
+        if self.region.Contains(*event.GetPosition()):
+            self.SetBackgroundColour(
+                    [min(255, c + 75) for c in self.color])
+            self.Refresh()
+        else:
+            self.SetBackgroundColour(self.color)
+            self.Refresh()
+            event.Skip()
