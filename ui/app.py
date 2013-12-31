3cc19f615960cd21b9435d7d5d1535eef86d6be9
Matt Keeter <matt.j.keeter@gmail.com>
2013-12-30 23:13:42 -0500

    We can now save and load files. (Unfortunately, loading files doesn't load appropriate UI elements)

--- ui/app.py
+++ ui/app.py
@@ -1,4 +1,5 @@
 import sys
+import cPickle as pickle
 
 from PySide import QtGui
 
@@ -33,6 +34,10 @@ class App(QtGui.QApplication):
             border: 0px;
         }""")
 
+        self.saved_state = None
+        self.filename = None
+        self.window = None
+
         self.make_actions()
 
         self.canvas = canvas.Canvas()
@@ -55,14 +60,77 @@ class App(QtGui.QApplication):
         self.saveas_action.setShortcuts(QtGui.QKeySequence.SaveAs)
         self.saveas_action.triggered.connect(self.on_saveas)
 
+    def clear(self):
+        """ Deletes all nodes, connections, and UI representations of same.
+        """
+        for n in node.base.nodes:
+            n.delete()
+        node.base.nodes = []
+        for n in self.canvas.findChildren(control.base.NodeControl):
+            n.deleteLater()
+        for c in self.canvas.findChildren(
+                control.connection.ConnectionControl):
+            c.deleteLater()
+        for c in self.canvas.findChildren(ui.editor.Editor):
+            c.deleteLater()
+
+    def get_state(self):
+        """ Return a serialized version of the scene
+            (used in undo/redo and for saving/loading).
+        """
+        return (node.base.serialize_nodes(),
+                node.connection.serialize_connections(node.base.nodes))
+
+    def unsaved(self):
+        return self.saved_state != self.get_state()
+
     def on_new(self):
-        pass
+        """ Creates a new file by clearing the old file.
+        """
+        self.clear()
+        self.filename = None
 
     def on_open(self):
-        pass
+        """ Opens a file, unpacking into a set of nodes and connections.
+        """
+        filename, filetype = QtGui.QFileDialog.getOpenFileName(
+                self.window, "Open", '', "*.sb")
+        if not filename:    return
+        try:
+            with open(filename, 'rb') as f:
+                state = pickle.load(f)
+                nodes = [node.base.Node.inflate(n) for n in state[0]]
+                connections = [node.connection.Connection.inflate(c, nodes)
+                               for c in state[1]]
+        except:
+            print "Failed to load file"
+        else:
+            print "I need to make UI controls for all of these:"
+            print nodes, connections
 
     def on_save(self):
-        pass
+        """ Saves a pickled representation of our current state.
+        """
+        if not self.filename:
+            return self.on_saveas()
+
+        state = self.get_state()
+        with open(self.filename, 'wb') as f:
+            pickle.dump(state, f)
+        self.saved_state = state
+
 
     def on_saveas(self):
-        pass
+        """ Select a filename, then assign it to self.filename and
+            call this class's on_save() function.
+        """
+        filename, filetype = QtGui.QFileDialog.getSaveFileName(
+                self.window, "Save as", '', "*.sb")
+        if filename:
+            self.filename = filename
+            return self.on_save()
+
+
+import control.base, control.connection
+import ui.editor
+import node.base
