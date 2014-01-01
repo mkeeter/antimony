a4d0d9309feee75c4bd3a8232b708882fa3b76c7
Matt Keeter <matt.j.keeter@gmail.com>
2014-01-01 15:10:01 -0500

    Switching from inheritance to composition for datum input modes

--- node/datum.py
+++ node/datum.py
@@ -1,15 +1,99 @@
+import operator
+
 import base
+
+################################################################################
+
+# These objects define different input schemes for Datum objects, including
+#   No inputs allowed
+#   One input allowed
+#   Multiple inputs allowed
+class NoInput(object):
+    """ Object that rejects all inputs.
+    """
+    def __init__(self, parent):     self.parent = parent
+    def __nonzero__(self):          return False
+    def __iter__(self):             return [].__iter__()
+    def accepts(self, conn):        return False
+    def size(self):                 return 0
+    def value(self):                return None
+    def connect(self, conn):
+        raise RuntimeError("Cannot connect to a NoInput object")
+    def disconnect(self, conn):
+        raise RuntimeError("Cannot disconnect from a NoInput object")
+
+class SingleInput(object):
+    """ Object that represents a single-source input system.
+    """
+    def __init__(self, parent):
+        self.parent = parent
+        self.i = None
+    def __nonzero__(self):  return self.i is not None
+    def value(self):        return self.i.source.value() if self.i else None
+    def size(self):
+        return 0 if self.i is None else 1
+    def __iter__(self):
+        return [].__iter__() if self.i is None else [self.i].__iter__()
+    def accepts(self, conn):
+        return (conn.source != self.parent and
+                conn.source.type == self.parent.type and
+                self.i is None)
+    def connect(self, conn):
+        self.i = conn
+        conn.target = self.parent
+        self.parent.sync()
+    def disconnect(self, conn):
+        self.i = None
+        conn.target = None
+        self.parent.sync()
+    def expr(self):
+        if self.i:  return self.i.source._expr
+        else:       return None
+
+class MultiInput(object):
+    """ Object that represents a multi-source input system.
+        Uses reduce with the given operator to combine inputs.
+    """
+    def __init__(self, parent, op=operator.or_):
+        self.parent = parent
+        self.i = []
+        self.op = op
+    def __nonzero__(self):  return self.i != []
+    def size(self):         return len(i)
+    def value(self):
+        if self.i:  return reduce(self.op, [i.source.value() for i in self.i])
+        else:       return None
+    def __iter__(self):
+        return self.i.__iter__()
+    def accepts(self, conn):
+        return (conn.source != self.parent and
+                conn.source.type == self.parent.type and
+                not conn.source in [c.source for c in self.i])
+    def connect(self, conn):
+        self.i.append(conn)
+        conn.target = self.parent
+        self.parent.sync()
+    def disconnect(self, conn):
+        self.i.remove(conn)
+        conn.target = None
+        self.parent.sync()
+    def expr(self):
+        if len(self.i) > 1:     return "%i inputs" % len(self.i)
+        elif len(self.i) == 1:  return "1 input"
+        else:                   return None
+
+################################################################################
 
 class Datum(object):
     stack = []
 
-    def __init__(self, node, expr, T):
+    def __init__(self, node, expr, T, inputType=SingleInput):
         self.node    = node
         self._expr   = str(expr)
         self.type    = T
 
         self.outputs = []
-        self.input   = None
+        self.input   = inputType(self)
 
         self.parents = set()
         self.children = set()
@@ -20,28 +104,22 @@ class Datum(object):
     def connections(self):
         """ Returns a list of connections attached to this datum.
         """
-        return self.outputs + ([self.input] if self.input else [])
+        return self.outputs + list(self.input)
 
     def can_connect(self, conn):
         """ Returns True if we can accept the given connection (as an input)
         """
-        return (self != conn.source and
-                self.type == conn.source.type and
-                self.input is None)
+        return self.input.accepts(conn)
 
     def connect_input(self, conn):
         """ Links an input connection to this node.
         """
-        self.input = conn
-        conn.target = self
-        self.sync()
+        self.input.connect(conn)
 
     def disconnect_input(self, conn):
         """ Disconnects an input connection.
         """
-        self.input = None
-        conn.target = None
-        self.sync()
+        self.input.disconnect(conn)
 
     def disconnect_output(self, conn):
         """ Disconnects an output connection.
@@ -52,13 +130,14 @@ class Datum(object):
     def get_expr(self):
         """ Returns the expression string.
         """
+        if self.input:  self._expr = self.input.expr()
         return self._expr
 
     def can_edit(self):
         """ Returns True if we can edit this datum
             (false if it's tied to an input)
         """
-        return self.input is None
+        return self.input.value() is None
 
     def set_expr(self, e):
         """ Sets the expression string.
@@ -123,8 +202,7 @@ class Datum(object):
         self.push_stack()
         try:
             if self.input:
-                self._expr = self.input.source._expr
-                t = self.input.source.value()
+                t = self.input.value()
             else:
                 t = eval(self._expr, base.dict())
                 if not isinstance(t, self.type):    t = self.type(t)
@@ -168,82 +246,15 @@ class NameDatum(Datum):
         """
         super(NameDatum, self).set_expr("'%s'" % e)
 
-################################################################################
-
-class MultiInputDatum(Datum):
-    """ Represents a node that can take in multiple inputs.
-    """
-    def __init__(self, node, value, T):
-        self.inputs = []
-        super(MultiInputDatum, self).__init__(node, value, T)
-        del self.input
-
-    def connections(self):
-        """ Returns a list of connections attached to this datum.
-        """
-        return self.outputs + self.inputs
-
-    def can_connect(self, conn):
-        """ Returns true if we can accept the given connection as an input
-        """
-        return (self != conn.source and self.type == conn.source.type
-                and not conn.source in [c.source for c in self.inputs])
-
-    def connect_input(self, conn):
-        """ Links an input connection to this node.
-        """
-        self.inputs.append(conn)
-        conn.target = self
-        self.sync()
-
-    def disconnect_input(self, conn):
-        """ Disconnects an input connection.
-        """
-        self.inputs.remove(conn)
-        conn.target = None
-        self.sync()
-
-    def get_expr(self):
-        """ Returns the expression string, or a placeholder if we have
-            multiple inputs.
-        """
-        if len(self.inputs) > 1:
-            return "%i inputs" % len(self.inputs)
-        elif len(self.inputs) == 1:
-            return "1 input"
-        else:
-            return self._expr
-
-    def can_edit(self):
-        """ Returns True if we can edit this datum
-            (false if it's tied to an input)
-        """
-        return self.inputs == []
 
 ################################################################################
 
 import operator
 
-class ExpressionDatum(MultiInputDatum):
+class ExpressionDatum(Datum):
     def __init__(self, node, value):
-        super(ExpressionDatum, self).__init__(node, value, Expression)
-
-    def eval(self):
-        """ Attempts to evaluate the expression and return a value.
-            Raises an exception if this fails.
-        """
-        self.push_stack()
-        try:
-            if self.inputs:
-                t = reduce(operator.or_, [i.source.value()
-                                          for i in self.inputs])
-            else:
-                t = eval(self._expr, base.dict())
-                if not isinstance(t, self.type):    t = self.type(t)
-        except:     raise
-        finally:    self.pop_stack()
-
-        return t
+        super(ExpressionDatum, self).__init__(
+                node, value, Expression, inputType=MultiInput)
 
 ################################################################################
 
@@ -254,14 +265,11 @@ class FunctionDatum(Datum):
 
     def __init__(self, node, function, T):
         self.function = function
-        super(FunctionDatum, self).__init__(node, None, T)
+        super(FunctionDatum, self).__init__(
+                node, None, T, inputType=NoInput)
 
-    def invalid(self):
-        raise TypeError("Invalid operation for output datum")
-    def can_connect(self, conn):        self.invalid()
-    def connect_input(self, conn):      self.invalid()
-    def disconnect_input(self, conn):   self.invalid()
-    def set_expr(self, value):          self.invalid()
+    def set_expr(self, value):
+        raise TypeError("set_expr is an invalid operation for a FunctionDatum")
 
     def get_expr(self):
         if self.valid():    return 'Function'
@@ -298,7 +306,7 @@ class FloatFunctionDatum(FunctionDatum):
 from fab.expression import Expression
 
 class ExpressionFunctionDatum(FunctionDatum):
-    """ Represents a math expression.
+    """ Represents a math expression calculated by a function.
     """
 
     def __init__(self, node, function):
