d9bc92e9cc21c70f39beaa030d1ded43629a248b
Matt Keeter <matt.j.keeter@gmail.com>
2014-01-18 13:04:11 -0500

    Header for triangulation routine

---
+++ fab/c/tree/triangulate.c
@@ -0,0 +1,9 @@
+#include "triangulate.h"
+
+#include "tree/tree.h"
+
+float* triangulate(MathTree* tree, const Region r, unsigned* const out)
+{
+    *out = 12;
+    return NULL;
+}
---
+++ fab/c/tree/triangulate.h
@@ -0,0 +1,10 @@
+#ifndef TRIANGULATE_H
+#define TRIANGULATE_H
+
+#include "util/region.h"
+
+struct MathTree_;
+
+float* triangulate(struct MathTree_* tree, Region r, unsigned* out);
+
+#endif
