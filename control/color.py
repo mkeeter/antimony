33631e1ef73888afeb4b48517b1166d898e2172f
Matt Keeter <matt.j.keeter@gmail.com>
2014-02-04 21:16:55 -0500

    Add color node and control

---
+++ control/color.py
@@ -0,0 +1,40 @@
+from PySide import QtCore, QtGui
+
+import point
+
+class ColorNodeControl(point.Point3DControl):
+    @classmethod
+    def new(cls, canvas, x, y, z, scale):
+        """ Constructs a new point at the given pocation.
+        """
+        p = ColorNode(get_name('c'), x, y, z)
+        return cls(canvas, p)
+
+    def __init__(self, canvas, target):
+        super(ColorNodeControl, self).__init__(canvas, target)
+        self.editor_datums = ['name','input','R','G','B','shape']
+
+    @property
+    def color(self):
+        return [self._cache[c]*255 for c in 'RGB']
+
+    def paint(self, painter, mask=False):
+        width, height = self.width(), self.height()
+
+        self.set_brush(painter, mask, self.color)
+
+        if mask:                        d = 22
+        elif self.drag_control.active:  d = 20
+        else:                           d = 14
+
+        painter.drawEllipse((width - d) / 2, (height - d) / 2, d, d)
+
+    def reposition(self):
+        """ Overload reposition so that it also re-renders this widget
+            (necessary for color changing).
+        """
+        super(ColorNodeControl, self).reposition()
+        self.update()
+
+from node.base import get_name
+from node.color import ColorNode
