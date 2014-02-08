f3dd5a72f396ac9c716f675079aba63ddf27f7c9
Matt Keeter <matt.j.keeter@gmail.com>
2014-02-08 15:00:19 -0500

    Basic highlighting

--- ui/script.py
+++ ui/script.py
@@ -1,4 +1,27 @@
 from PySide import QtCore, QtGui
+import keyword
+from control import colors
+
+class _Highlighter(QtGui.QSyntaxHighlighter):
+    def __init__(self, parent):
+        super(_Highlighter, self).__init__(parent)
+        self.rules = []
+
+        keyword_format = QtGui.QTextCharFormat()
+        keyword_format.setForeground(QtGui.QColor(*colors.blue))
+
+        for k in keyword.kwlist:
+            self.rules.append((QtCore.QRegExp(k), keyword_format))
+
+
+    def highlightBlock(self, text):
+        for r in self.rules:
+            index = r[0].indexIn(text)
+            while index >= 0:
+                length = r[0].matchedLength()
+                self.setFormat(index, length, r[1])
+                index = r[0].indexIn(text, index + length)
+
 
 class ScriptEditor(QtGui.QTextEdit):
     def __init__(self, parent):
@@ -7,4 +30,6 @@ class ScriptEditor(QtGui.QTextEdit):
         font = QtGui.QFont()
         font.setFamily("Courier")
         self.setFont(font)
+
+        _Highlighter(self.document())
 
