436e9e30546020028daab38c5a4d4181259a6876
Matt Keeter <matt.j.keeter@gmail.com>
2014-02-09 13:57:02 -0500

    Better default script

--- node/script.py
+++ node/script.py
@@ -2,9 +2,18 @@ import base
 import datum
 
 class ScriptNode(base.Node3D):
+    default = """import math
+
+from fab.shapes import *
+from fab.expression import Expression
+
+c = circle(0, 0, 1)
+output(c)
+"""
+
     def __init__(self, name, x, y, z):
         super(ScriptNode, self).__init__(name, x, y, z)
-        self.add_datum('script', datum.ScriptDatum(self, 'print "hello"'))
+        self.add_datum('script', datum.ScriptDatum(self, self.default))
 
     def get_control(self):
         import control.script
