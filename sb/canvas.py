b08926a64fba2c09e25acc977dbb2aa34da90de5
Matt Keeter <matt.j.keeter@gmail.com>
2014-05-17 12:21:13 -0400

    Beginning to work on canvas rotation

--- sb/canvas.py
+++ sb/canvas.py
@@ -1,6 +1,10 @@
-from PySide import QtGui
+from PySide import QtCore, QtGui
 
 class Canvas(QtGui.QGraphicsView):
+    rotated = QtCore.Signal(QtGui.QMatrix4x4)
+
     def __init__(self, parent=None):
         self.scene = QtGui.QGraphicsScene(parent)
         super(Canvas, self).__init__(self.scene, parent)
+        self.setStyleSheet('QGraphicsView { border-style: none; }')
+        self.setBackgroundBrush(QtCore.Qt.black)
