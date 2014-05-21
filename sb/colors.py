76adb3159cd536f7249013ecf5d3a3205630a942
Matt Keeter <matt.j.keeter@gmail.com>
2014-05-21 13:37:55 -0400

    Create dim and highlighted colors

--- sb/colors.py
+++ sb/colors.py
@@ -16,3 +16,18 @@ violet  = "#6c71c4"
 blue    = "#268bd2"
 cyan    = "#2aa198"
 green   = "#859900"
+
+def _highlight(color, scale=1.2):
+    r = min(255, int(eval('0x%s' % color[1:3]) * scale))
+    g = min(255, int(eval('0x%s' % color[3:5]) * scale))
+    b = min(255, int(eval('0x%s' % color[5:7]) * scale))
+
+    return ('#%2x%2x%2x' % (r, g, b)).replace(' ', '0')
+
+# Automatically generate highlighted and dimmed versions
+# of the colors
+colors = list(locals().iterkeys())
+for c in colors:
+    if not c.startswith('__') and isinstance(eval(c), str):
+        exec('%s_h = "%s"' % (c, _highlight(eval(c))))
+        exec('%s_d = "%s"' % (c, _highlight(eval(c), 0.8)))
