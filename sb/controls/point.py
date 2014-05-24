5a067f9e61e33c160d5f721060987487740cd83a
Matt Keeter <matt.j.keeter@gmail.com>
2014-05-24 15:12:27 -0400

    Adding Point2D object and control

--- sb/controls/point.py
+++ sb/controls/point.py
@@ -3,7 +3,6 @@ from PySide import QtCore, QtGui
 from sb.controls.control import Control
 
 class Point3DControl(Control):
-
     @property
     def pos(self):
         return QtGui.QVector3D(
@@ -27,6 +26,41 @@ class Point3DControl(Control):
 
     def drag(self, c, d):
         for a in 'xyz':
+            if self._node.object_datums[a].simple():
+                self._node.object_datums[a] += getattr(d, a)()
+
+    def update_center(self):
+        """ Recalculates viewport coordinates where the node viewer should be
+            positioned, then emits center_changed with that position.
+        """
+        pt = self.transform_points([self.pos])[0]
+        self.center_changed.emit(QtCore.QPoint(pt.x(), pt.y()))
+
+################################################################################
+
+class Point2DControl(Control):
+    @property
+    def pos(self):
+        return QtGui.QVector3D(
+                self._node.object_datums['x']._value,
+                self._node.object_datums['y']._value, 0)
+
+    def boundingRect(self):
+        return self.bounding_box([self.pos])
+
+    def paint(self, painter, options, widget):
+        self.set_default_pen(painter)
+        self.set_default_brush(painter)
+        painter.drawPath(self.shape())
+
+    def shape(self):
+        pt = self.transform_points([self.pos])[0]
+        path = QtGui.QPainterPath()
+        path.addEllipse(pt.x() - 5, pt.y() - 5, 10, 10)
+        return path
+
+    def drag(self, c, d):
+        for a in 'xy':
             if self._node.object_datums[a].simple():
                 self._node.object_datums[a] += getattr(d, a)()
 
--- sb/nodes/point.py
+++ sb/nodes/point.py
@@ -1,5 +1,5 @@
-from sb.nodes.node import Node3D
-from sb.controls.point import Point3DControl
+from sb.nodes.node import Node3D, Node2D
+from sb.controls.point import Point3DControl, Point2DControl
 
 class Point3D(Node3D):
     menu_name = "Point (3D)"
@@ -10,3 +10,15 @@ class Point3D(Node3D):
 
     def __init__(self, name, x, y, z, scale):
         super(Point3D, self).__init__(name, x, y, z)
+
+################################################################################
+
+class Point2D(Node2D):
+    menu_name = "Point (2D)"
+    menu_category = "2D"
+
+    name_prefix = 'p'
+    _control = Point2DControl
+
+    def __init__(self, name ,x, y, z, scale):
+        super(Point2D, self).__init__(name, x, y)
