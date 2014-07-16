8f223417f3e96db881b1002ec1e16a863b61b54c
Matt Keeter <matt.j.keeter@gmail.com>
2014-07-16 18:57:22 -0400

    QImage-based compositing.
    Still too slow -- will switch to height-maps until OpenGL comes along.

--- src/ui/depth_image.cpp
+++ src/ui/depth_image.cpp
@@ -23,32 +23,61 @@ void DepthImageItem::paint(QPainter *painter,
                            QWidget *widget)
 {
     QPoint a = canvas->mapFromScene(mapToParent(0, 0).toPoint());
+    QImage& zbuffer = *canvas->getDepth();
 
-    QImage blended(depth.width(), depth.height(), QImage::Format_RGBA8888);
-    blended.fill(0);
-
-    QImage* buffer = canvas->getDepth();
-    for (int j=0; j < depth.height(); ++j)
+    // We need to transform the depth image into global z space
+    QImage depth_(zbuffer.width(), zbuffer.height(), zbuffer.format());
+    depth_.fill(0x0); // dummy scale for now
     {
-        if (a.y() + j < 0 || a.y() + j >= buffer->height())
-        {
-            continue;
-        }
-        for (int i=0; i < depth.width(); ++i)
-        {
-            if (a.x() + i < 0 || a.x() + i >= buffer->width())
-            {
-                continue;
-            }
+        QPainter p(&depth_);
 
-            if (depth.pixel(i, j) > buffer->pixel(i, j))
-            {
-                uint32_t pix = shaded.pixel(i, j);
-                blended.setPixel(i, j, pix | pix << 8 | pix << 16);
-                buffer->setPixel(a.x() + i, a.y() + j, depth.pixel(i, j));
-            }
-        }
+        // Apply scale to depth-map
+        p.setCompositionMode(QPainter::CompositionMode_Multiply);
+        p.drawImage(a.x(), a.y(), depth);
+
+        // Apply scale
+        QImage scale(depth.width(), depth.height(), depth.format());
+        scale.fill(0xffffff);
+        p.setCompositionMode(QPainter::CompositionMode_Multiply);
+        p.drawImage(a.x(), a.y(), scale);
+
+        // Apply offset
+        QImage offset(depth.width(), depth.height(), depth.format());
+        offset.fill(0x0); // dummy offset for now
+        p.setCompositionMode(QPainter::CompositionMode_Plus);
+        p.drawImage(a.x(), a.y(), offset);
+
+        // Multiply by a basic mask so that we have zeros outside of original
+        p.setCompositionMode(QPainter::CompositionMode_Multiply);
+        p.drawImage(a.x(), a.y(),
+                    depth.createMaskFromColor(0xff000000, Qt::MaskOutColor));
     }
-    buffer->save("out.png");
-    painter->drawImage(0, 0, blended);
+
+    QImage mask = zbuffer;
+    // Update buffer with lightened height-map.
+    {
+        QPainter p(&zbuffer);
+        p.setCompositionMode(QPainter::CompositionMode_Lighten);
+        p.drawImage(0, 0, depth_);
+    }
+
+    zbuffer.save("zbuffer.png");
+
+    // Subtract old buffer from new buffer.
+    {
+        QPainter p(&mask);
+        p.setCompositionMode(QPainter::CompositionMode_Difference);
+        p.drawImage(0, 0, zbuffer);
+    }
+
+    mask = mask.createMaskFromColor(0, Qt::MaskOutColor);
+
+    QImage masked = shaded;
+    {
+        QPainter p(&masked);
+        p.setCompositionMode(QPainter::CompositionMode_Multiply);
+        p.drawImage(0, 0, mask);
+    }
+
+    painter->drawImage(0, 0, masked);
 }
