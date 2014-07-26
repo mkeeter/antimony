68f0e51303fd31c685253c489eddbae6ff6b50c4
Matt Keeter <matt.j.keeter@gmail.com>
2014-07-26 12:16:40 -0400

    More work on serializer/deserializer

---
+++ src/node/deserializer.cpp
@@ -0,0 +1,4 @@
+#include <Python.h>
+#include "node/deserializer.h"
+
+
---
+++ src/node/deserializer.h
@@ -0,0 +1,25 @@
+#ifndef DESERIALIZER_H
+#define DESERIALIZER_H
+
+#include <QObject>
+
+class Datum;
+class Node;
+
+class SceneDeserializer : public QObject
+{
+    Q_OBJECT
+public:
+    explicit SceneDeserializer(QObject* parent=0);
+    QDataStream run(QDataStream* in);
+    bool hasError() const { return failed; }
+    QString errorMessage() const { return error_message; }
+protected:
+    void deserializeNodes(QDataStream* in, QObject* p);
+    void deserializeNode(QDataStream* in, Node* node);
+
+    bool failed;
+    QString error_message;
+};
+
+#endif // DESERIALIZER_H
