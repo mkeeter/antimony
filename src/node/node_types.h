3c138c7ee45dfbfee0a3bb9228d48b367d0737e3
Matt Keeter <matt.j.keeter@gmail.com>
2014-07-26 11:30:55 -0400

    Oh, this header would help.

---
+++ src/node/node_types.h
@@ -0,0 +1,15 @@
+#ifndef NODE_TYPES_H
+#define NODE_TYPES_H
+
+namespace NodeType
+{
+    enum NodeType {
+        CIRCLE,
+        CUBE,
+        POINT3D,
+        SCRIPT,
+    };
+};
+
+
+#endif // NODE_TYPES_H
