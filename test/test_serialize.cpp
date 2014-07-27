e7d79df0b7b98c3dbfaace50f0501b112678dfc0
Matt Keeter <matt.j.keeter@gmail.com>
2014-07-27 13:34:04 -0400

    Simple unit test for deserialization

--- test/test_serialize.cpp
+++ test/test_serialize.cpp
@@ -5,8 +5,22 @@
 
 #include "test_serialize.h"
 #include "node/2d/circle_node.h"
+#include "node/manager.h"
 
 void TestSerialize::SerializeSimpleScene()
 {
-    QVERIFY(true);
+    QByteArray empty = NodeManager::manager()->getSerializedScene();
+
+    Node* c = new CircleNode("c", "0.0", "0.0", "1.0");
+    QByteArray out = NodeManager::manager()->getSerializedScene();
+
+    NodeManager::manager()->clear();
+    c = NULL;
+
+    QCOMPARE(empty, NodeManager::manager()->getSerializedScene());
+
+    NodeManager::manager()->deserializeScene(out);
+    QCOMPARE(out,  NodeManager::manager()->getSerializedScene());
+
+    NodeManager::manager()->clear();
 }
