64888145f04a5a882c3e4e7de53beb591399aaaa
Matt Keeter <matt.j.keeter@gmail.com>
2014-08-24 19:20:48 -0400

    Add Q_UNUSED for painter

--- src/ui/depth_image.cpp
+++ src/ui/depth_image.cpp
@@ -80,6 +80,7 @@ void DepthImageItem::paint(QPainter *painter,
                            const QStyleOptionGraphicsItem *option,
                            QWidget *widget)
 {
+    Q_UNUSED(painter);
     Q_UNUSED(option);
     Q_UNUSED(widget);
 
