abd4239ba9afd527c5c66ccba8bc0dbe5d6512c2
Matt Keeter <matt.j.keeter@gmail.com>
2015-03-01 11:58:47 -0500

    Turn off output ports for ShapeDatums to prevent them from rendering

--- src/graph/datum/datums/shape_datum.h
+++ src/graph/datum/datums/shape_datum.h
@@ -15,6 +15,10 @@ public:
     PyTypeObject* getType() const override { return fab::ShapeType; }
     DatumType::DatumType getDatumType() const override
         { return DatumType::SHAPE; }
+
+    // Always return false for hasOutput, otherwise that output port
+    // will be seen as something that should be rendered.
+    bool hasOutput() const { return false; }
 };
 
 #endif // SHAPE_DATUM_H
