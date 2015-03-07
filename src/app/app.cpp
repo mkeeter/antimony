ac59a693a4374fafe60987738ae5ad816faf2ba4
Matt Keeter <matt.j.keeter@gmail.com>
2015-03-06 19:19:33 -0500

    Adding auto-upgrade using old deserializer

--- src/app/app.cpp
+++ src/app/app.cpp
@@ -34,6 +34,7 @@
 #include "graph/datum/link.h"
 #include "graph/node/serializer.h"
 #include "graph/node/deserializer.h"
+#include "graph/node/deserializer_old.h"
 
 #include "fab/types/shape.h"
 
@@ -204,10 +205,19 @@ void App::loadFile(QString f)
 
     if (ds.failed == true)
     {
-        QMessageBox::critical(NULL, "Loading error",
-                "<b>Loading error:</b><br>" +
-                ds.error_message);
-        onNew();
+        // Attempt to load the file with the old deserializer
+        file.reset();
+        QDataStream in(&file);
+        SceneDeserializerOld dso(root);
+        dso.run(&in);
+
+        if (dso.failed)
+        {
+            QMessageBox::critical(NULL, "Loading error",
+                    "<b>Loading error:</b><br>" +
+                    dso.error_message);
+            onNew();
+        }
     } else {
         // If there's a warning message, show it in a box.
         if (!ds.warning_message.isNull())
