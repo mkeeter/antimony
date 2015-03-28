703f1727e74f7c015026adcf03ea2e31e07756fa
Matt Keeter <matt.j.keeter@gmail.com>
2015-03-28 12:41:59 -0400

    Insert a (non-positioned) vertex for edge and corner fans

--- src/fab/tree/triangulate/mesher.cpp
+++ src/fab/tree/triangulate/mesher.cpp
@@ -419,41 +419,79 @@ std::list<Vec3f> Mesher::get_contour()
             }
         }
     }
+
+    // Remove the last point of the contour, since it's a closed loop.
+    contour.pop_back();
     return contour;
 }
 
 void Mesher::check_feature()
 {
-    const auto contour = get_contour();
+    auto contour = get_contour();
     const auto normals = get_normals(contour);
 
     // Find the largest cone and the normals that enclose
     // the largest angle as n0, n1.
     float theta = 1;
     Vec3f n0, n1;
-    for (auto i : normals)
+    for (auto ni : normals)
     {
-        for (auto j : normals)
+        for (auto nj : normals)
         {
-            float dot = i.dot(j);
+            float dot = ni.dot(nj);
             if (dot < theta)
             {
                 theta = dot;
-                n0 = i;
-                n1 = j;
+                n0 = ni;
+                n1 = nj;
             }
         }
     }
 
+    // If there isn't a feature in this fan, then return immediately.
     if (theta > 0.9)
         return;
 
+    // Decide whether this is a corner or edge feature.
     const Vec3f nstar = n0.cross(n1);
     float phi = 0;
-    for (auto i : normals)
-        phi = fmax(phi, fabs(nstar.dot(i)));
-
+    for (auto n : normals)
+        phi = fmax(phi, fabs(nstar.dot(n)));
     bool edge = phi < 0.7;
+
+    // Find the center of the contour.
+    Vec3f center(0, 0, 0);
+    for (auto c : contour)
+        center += c;
+    center /= contour.size();
+
+
+    // Construct one of the matrices for use in our least-square fit.
+    /*
+    Eigen::MatrixX3d A;
+    int i=0;
+    for (auto n : normals)
+        A.row(i++) << n;
+        */
+
+    // Erase this triangle fan, as we'll be inserting a vertex in the center.
+    triangles.erase(fan_start, triangles.end());
+
+    contour.push_back(contour.front());
+    Vec3f new_pt = center;
+
+    // Construct a new triangle fan.
+    auto p0 = contour.begin();
+    auto p1 = contour.begin();
+    p1++;
+    while (p1 != contour.end())
+        triangles.push_back(Triangle(*(p0++), *(p1++), new_pt));
+
+    /*
+    Eigen::Matrix1Xd B;
+    i = 0;
+    for (auto 
+    */
 
     printf("%f %f\n", theta, phi);
 }
