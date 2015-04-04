15f82b31b08b4115417c77e772b2ce4a5e1d49d5
Matt Keeter <matt.j.keeter@gmail.com>
2015-04-03 20:57:26 -0400

    Helper function to get std::map-able vertices

--- src/fab/tree/triangulate/triangle.cpp
+++ src/fab/tree/triangulate/triangle.cpp
@@ -1,5 +1,26 @@
 #include "tree/triangulate/triangle.h"
 
+std::array<float, 3> Triangle::a_() const
+{
+    return {{static_cast<float>(a[0]),
+             static_cast<float>(a[1]),
+             static_cast<float>(a[2])}};
+}
+
+std::array<float, 3> Triangle::b_() const
+{
+    return {{static_cast<float>(b[0]),
+             static_cast<float>(b[1]),
+             static_cast<float>(b[2])}};
+}
+
+std::array<float, 3> Triangle::c_() const
+{
+    return {{static_cast<float>(c[0]),
+             static_cast<float>(c[1]),
+             static_cast<float>(c[2])}};
+}
+
 Vec3f Triangle::normal() const
 {
     return (b - a).cross(c - a);
--- src/fab/tree/triangulate/triangle.h
+++ src/fab/tree/triangulate/triangle.h
@@ -3,6 +3,8 @@
 
 #include "Eigen/Dense"
 
+#include <array>
+
 typedef Eigen::Vector3d Vec3f;
 
 struct Triangle {
@@ -10,6 +12,10 @@ struct Triangle {
 
     Triangle(Vec3f a, Vec3f b, Vec3f c) :
         a(a), b(b), c(c) {}
+
+    std::array<float, 3> a_() const;
+    std::array<float, 3> b_() const;
+    std::array<float, 3> c_() const;
 
     /*
      *  Calculate the normal of this triangle.
