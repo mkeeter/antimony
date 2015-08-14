7ada6ebb9ac044354d9896d47d8cd5fe6e8acb51
ApostolosB <barz621@gmail.com>
2015-08-14 12:18:07 +0300

    Update viewport.cpp
    Fixes the transparent view window on wayland

--- app/src/ui/viewport/viewport.cpp
+++ app/src/ui/viewport/viewport.cpp
@@ -584,7 +584,7 @@ void Viewport::drawBackground(QPainter* painter, const QRectF& rect)
     QGraphicsView::drawBackground(painter, rect);
 
     painter->beginNativePainting();
-    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glClearColor(0.0, 0.0, 0.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     for (auto d : findChildren<DepthImageItem*>())
