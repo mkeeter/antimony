45b0dd557375c208f2cd7a82a28013f0aa4fcebd
Matt Keeter <matt.j.keeter@gmail.com>
2015-09-27 18:49:43 -0400

    Assert the dataflow tracking works with subgraphs

--- lib/graph/tests/subgraph.cpp
+++ lib/graph/tests/subgraph.cpp
@@ -5,7 +5,7 @@
 #include "graph/graph.h"
 #include "graph/graph_node.h"
 
-TEST_CASE("Graph node lookups")
+TEST_CASE("Subgraph datum lookups")
 {
     auto g = new Graph();
     auto a = new GraphNode("a", g);
@@ -31,6 +31,28 @@ TEST_CASE("Graph node lookups")
         REQUIRE(by->getError().find("Name '__parent' is not defined")
                 != std::string::npos);
     }
+
+    delete g;
+}
+
+TEST_CASE("Subgraph value tracking")
+{
+    auto g = new Graph();
+    auto a = new GraphNode("a", g);
+    auto ax = new Datum("x", "3.0", &PyFloat_Type, a);
+
+    auto sub = a->getGraph();
+    auto b = new Node("b", sub);
+
+    auto bx = new Datum("x", Datum::SIGIL_CONNECTION +
+                             std::string("[__parent.__0]"),
+                        &PyFloat_Type, b);
+    REQUIRE(bx->currentValue() != NULL);
+    REQUIRE(PyFloat_AsDouble(bx->currentValue()) == 3.0);
+
+    ax->setText("2.0");
+    REQUIRE(bx->currentValue() != NULL);
+    REQUIRE(PyFloat_AsDouble(bx->currentValue()) == 2.0);
 
     delete g;
 }
