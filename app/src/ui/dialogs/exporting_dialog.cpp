0eba975c16144e372989ac27f58057e0e34c30a6
Matt Keeter <matt.j.keeter@gmail.com>
2016-03-13 18:48:02 -0400

    app is dead, long live app

--- app/src/ui/dialogs/exporting_dialog.cpp
+++
@@ -1,9 +0,0 @@
-#include "ui_exporting_dialog.h"
-#include "ui/dialogs/exporting_dialog.h"
-
-ExportingDialog::ExportingDialog(QWidget* parent)
-    : QDialog(parent, Qt::CustomizeWindowHint|Qt::WindowTitleHint),
-      ui(new Ui::ExportingDialog)
-{
-    ui->setupUi(this);
-}
